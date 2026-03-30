c5f0003c5507b1f67e323d4c02f8cfaef59ffe8d
Dalete Ferreira <daleteferreirac@gmail.com>
2026-03-29 22:58:54 -0300

    feat: add relative SASA calculation per residue

--- src/sasa.py
+++ src/sasa.py
@@ -1,6 +1,14 @@
 from Bio.PDB import PDBParser
 from Bio.PDB.SASA import ShrakeRupley
 
+MAX_SASA = {
+    "ALA": 129, "VAL": 174, "LEU": 201, "ILE": 197,
+    "MET": 224, "PHE": 240, "TRP": 285, "PRO": 159,
+    "SER": 155, "THR": 172, "ASN": 195, "GLN": 223,
+    "TYR": 263, "CYS": 167,
+    "ARG": 274, "LYS": 236, "HIS": 224, "ASP": 193, "GLU": 223
+}
+
 def compute_sasa_per_residue(pdb_file):
     """
     Computes SASA per residue using Shrake–Rupley algorithm.
@@ -26,8 +34,18 @@ def compute_sasa_per_residue(pdb_file):
         chain = residue.get_parent().id # Chain identifier (e.g. 'A')
         res_num = residue.id[1]  # already int
 
+        res_name = residue.get_resname()
         sasa = residue.sasa
-        sasa_per_residue[(chain, res_num)] = sasa
+
+        if res_name in MAX_SASA:
+            relative = sasa / MAX_SASA[res_name]
+        else:
+            relative = None  # fallback for unknown residues
+
+        sasa_per_residue[(chain, res_num)] = {
+            "absolute": sasa,
+            "relative": relative
+        }
         total_sasa += sasa
 
     return total_sasa, sasa_per_residue
@@ -37,4 +55,8 @@ total_sasa, sasa_res = compute_sasa_per_residue("../data/1A6M.pdb")
 print(f"Total SASA: {total_sasa:.2f} Å²")
 
 for key, value in list(sasa_res.items())[:10]:
-    print(key, f"{value:.2f}")
+    print(
+        key,
+        f"abs={value['absolute']:.2f}",
+        f"rel={value['relative']:.2f}" if value["relative"] else "rel=None"
+    )
